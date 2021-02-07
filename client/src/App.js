1645f47cbba9c8271747be02c1a8c2d4d16c5c6b
chemseddean <bensafiachemseddine@gmail.com>
2021-02-07 22:47:51 +0100

    Imports update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import React, {Fragment} from 'react'
+import React, {Component,Fragment} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
-import Register from './components/layout/Register'
-import Login from './components/layout/Login'
-import Alert from '.components/layout/Alert'
-
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import Alert from './components/layout/Alert'
 //Redux
 import {Provider} from 'react-redux'
 import store from './store'
