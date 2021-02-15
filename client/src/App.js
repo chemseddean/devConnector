9e8842747b8261380228f60419cdd1c57f5290f7
chemseddean <bensafiachemseddine@gmail.com>
2021-02-15 23:24:46 +0100

    Private route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import {loadUser} from './actions/auth'
 import setAuthToken from './utils/setAuthToken'
 
 import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
 
 
 if(localStorage.token){
@@ -35,7 +36,7 @@ const App = () => {
         <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
-            <Route exact path='/dashboard' component={Dashboard} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
         </Switch>
       </section>
     </Fragment>
