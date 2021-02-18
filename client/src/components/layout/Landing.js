3ab4ead8a33346558a65ceb2dbd4af8924f7edbf
chemseddean <bensafiachemseddine@gmail.com>
2021-02-18 23:31:17 +0100

    Dashboard Landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
-export const Landing = () => {
-    return (
-        <section className="landing">
+export const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated){
+    return <Redirect to='dashboard' />
+  }  
+  
+  return (
+      <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
           <h1 className="x-large">Developer Connector</h1>
@@ -21,4 +27,11 @@ export const Landing = () => {
     )
 }
 
-export default Landing
+Landing.propTypes = {
+  isAuthenticated : PropTypes.bool,
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+export default connect(mapStateToProps)(Landing)
