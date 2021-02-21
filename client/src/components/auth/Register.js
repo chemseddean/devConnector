6fe6b657a07671b50a7c5a01a675580cc3c128a5
chemseddean <bensafiachemseddine@gmail.com>
2021-02-22 00:58:28 +0100

    CreateProfile form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -103,4 +103,4 @@ const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated
 })
 
-export default connect(null, { setAlert, register })(Register)
+export default connect(mapStateToProps, { setAlert, register })(Register)
